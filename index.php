8d8a53089a332143431bb7f85b6af60a22f51602
rahulgaikwad1257 <122210424+rahulgaikwad1257@users.noreply.github.com>
2023-01-08 18:22:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="refresh" content="0; URL=http://mahavahn.com/88/certificates/search.php?qr_code=290CTTRFCTQYE6OCQXN9" />
+    <meta http-equiv="refresh" content="0; URL=http://mahavahn.com/88/certificates/search.php?qr_code=290CTTRFCTQYE6OCQX9" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
